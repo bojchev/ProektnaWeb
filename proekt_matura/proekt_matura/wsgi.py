56589411dbbdb7939174ae44150ae46d6ad81093
bojchev <nikolabojcev2008@gmail.com>
2026-03-03 22:07:11 +0100

    proekt i apps hiearhija

---
+++ proekt_matura/proekt_matura/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for proekt_matura project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proekt_matura.settings')
+
+application = get_wsgi_application()
