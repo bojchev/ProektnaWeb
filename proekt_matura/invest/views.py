e87e54ace99b73cf0cc3418f70ff0c350b92a75b
bojchev <nikolabojcev2008@gmail.com>
2026-03-28 22:45:36 +0100

    commit

--- proekt_matura/invest/views.py
+++ proekt_matura/invest/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import JsonResponse
-from django.utils import timezone
 from decimal import Decimal
-import json
 
 from .models import Portfolio, Security, Holding, Transaction
 
